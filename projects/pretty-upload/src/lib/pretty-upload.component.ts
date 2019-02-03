61bea84c5e582aa9e58a5809df64b002b359898a
Rafael Barbosa <rafaelmilanib@gmail.com>
2019-02-03 02:08:03 -0200

    add environments e fix changes.

--- projects/pretty-upload/src/lib/pretty-upload.component.ts
+++ projects/pretty-upload/src/lib/pretty-upload.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { Observable } from 'rxjs';
+import { finalize } from 'rxjs/operators';
+import { AngularFireStorage } from '@angular/fire/storage';
 
 @Component({
   selector: 'pretty-upload',
@@ -6,12 +9,29 @@ import { Component, OnInit } from '@angular/core';
     <p>
       pretty-upload works!
     </p>
+    <input type="file" (change)="uploadFile($event)">
   `,
   styles: []
 })
 export class PrettyUploadComponent implements OnInit {
+  uploadPercent: Observable<number>;
+  downloadURL: Observable<string>;
+  constructor(private storage: AngularFireStorage) { }
 
-  constructor() { }
+  uploadFile(event) {
+    const file = event.target.files[0];
+    const filePath = 'test-path';
+    const fileRef = this.storage.ref(filePath);
+    const task = this.storage.upload(filePath, file);
+
+    // observe percentage changes
+    this.uploadPercent = task.percentageChanges();
+    // get notified when the download URL is available
+    task.snapshotChanges().pipe(
+        finalize(() => this.downloadURL = fileRef.getDownloadURL() )
+    )
+      .subscribe();
+  }
 
   ngOnInit() {
   }
--- projects/pretty-upload/src/lib/pretty-upload.module.ts
+++ projects/pretty-upload/src/lib/pretty-upload.module.ts
@@ -1,8 +1,12 @@
 import { NgModule } from '@angular/core';
 import { PrettyUploadComponent } from './pretty-upload.component';
+import { AngularFireModule } from '@angular/fire';
+import { environment } from '../../../../src/environments/environment';
 
 @NgModule({
   imports: [
+    PrettyUploadComponent,
+    AngularFireModule.initializeApp(environment.firebase),
   ],
   declarations: [PrettyUploadComponent],
   exports: [PrettyUploadComponent]
